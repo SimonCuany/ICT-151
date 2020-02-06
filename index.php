8930b414991d34ae5a113ce2aa13b6f84a3aab2a
SimonCuany <47849679+SimonCuany@users.noreply.github.com>
2020-02-06 15:53:28 +0100

    Update sql --> php

--- index.php
+++ index.php
@@ -6,4 +6,20 @@
  * Time   : 13:57
  */
 
+$user = "PHPApplication";
+
+$pass = "Pa$\$w0rd";
+
+try {
+
+    $dbh = new PDO('mysql:host=localhost;dbname=mcu', $user, $pass);
+    foreach ($dbh->query('SELECT * from actors') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
 ?>
