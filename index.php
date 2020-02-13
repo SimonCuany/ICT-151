9536a0d63be333e8eb11cac4bddb53578e946303
SimonCuany <47849679+SimonCuany@users.noreply.github.com>
2020-02-13 15:59:46 +0100

    Try to work - don't work

--- index.php
+++ index.php
@@ -17,53 +17,101 @@ function getAllItems()
         $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
         $query = 'SELECT * FROM filmmakers'; //initalise the Query variable and the commande to execute
         $statement = $dbh->prepare($query);//Prepare Query
-        $statement ->execute();
+        $statement->execute();
         $queryResult = $statement->fetchAll(); //prepare result for client
         $dbh = null;
         return $queryResult;
     } catch (PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
-        die();
+
     }
 }
 
-function getoneItem() {
+function getoneItem()
+{
 
     require ".constant.php";
 
     try {
 
         $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
-        $query = 'SELECT * FROM filmmakers'; //initalise the Query variable and the commande to execute
+        $query = 'SELECT * FROM filmMakers'; //initalise the Query variable and the commande to execute
         $statement = $dbh->prepare($query);//Prepare Query
-        $statement ->execute();
+        $statement->execute();
         $queryResult = $statement->fetchAll(); //prepare result for client
         $dbh = null;
         return $queryResult;
     } catch (PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
-        die();
+
     }
 
 }
+
+
+function updateFilmMaker($filmMakers)
+{
+
+
+    try {
+
+        require ".constant.php";
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+        $query = "UPDATE filmMakers SET lastname = 'Toto', nationality = 'Espagol' WHERE id = $id"; //initalise the Query variable and the commande to execute
+        $statement = $dbh->prepare($query);//Prepare Query
+        $statement->execute();
+        $queryResult = $statement->fetch(); //prepare result for client
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+
+    }
+}
+
+
+//######################################### Test unitaire ###############################################\\
+
+
+require ".constant.php";
+$cmd = "mysql -u $user -p$pass < Restore-MCU-PO-Final.sql";
+exec($cmd);
+
 // Test unitaire de la fonction getAllItems
 $Items = getAllItems();
 if (count($Items) == 4) {
     echo "Test unitaire de la fonction getAllItems : ";
-    echo "OK MON POTE
-";
+    echo "OK MON POTE ";
 } else {
     echo "Test unitaire de la fonction getAllItems : ";
-    echo "PAS OKE MON POTE";
-}
+    echo "PAS OK MON POTE";
 
-//Test unitaire
+}
+echo "\n";
+
+//Test unitaire de la fonction getoneItem
 $Item = getoneItem();
 if (count($Item) == 4) {
     echo "Test unitaire de la fonction GetoneItem : ";
-    echo "OK MON POTE";
+    echo "OK MON POTE ";
 } else {
     echo "Test unitaire de la fonction GetoneItem : ";
-    echo "PAS OKE MON POTE";
+    echo "PAS OK MON POTE";
 }
+
+echo "\n";
+
+//Test unitaire de la fonction updateFilmMaker
+$Item = updateFilmMaker(2);
+if ($Item('lastname') == 'Toto') {
+    echo "Test unitaire de la fonction updateFilmMaker : ";
+    echo "OK MON POTE ";
+} else {
+    echo "Test unitaire de la fonction updateFilmMaker : ";
+    echo "PAS OK MON POTE";
+
+}
+
+die();
+
 ?>
